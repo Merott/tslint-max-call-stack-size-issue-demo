804fb4b1bbba63955a60fef53f7c94de881dc88a
Merott Movahedi <merott@merott.com>
2017-04-27 20:19:25 +0100

    minimal repo to show issue

---
+++ README.md
@@ -0,0 +1,6 @@
+# tslint-max-call-stack-size-issue-demo
+
+- `yarn`
+- `npm run lint`
+
+See https://github.com/palantir/tslint/issues/2646.
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('app', () => {
+  it(`should know numbers`, () => {
+    expect(1).to.eq(1);
+  });
+});
